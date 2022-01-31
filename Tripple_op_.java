52a31305ecde6908d8f537eeb86a93a874d56771
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 11:10:55 +0800

    Add files via upload

---
+++ Tripple_op_.java
@@ -0,0 +1,25 @@
+package Demo03;
+
+public class Tripple_op_ {
+    public static void main(String[] args) {
+        int a = 3;
+        int b =5;
+        int max = (a>b)? a:b;
+        System.out.println(max);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
