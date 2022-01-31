12ec2d974a8d0a6de3e5b76c60f2fbc3169612b1
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 11:07:16 +0800

    Add files via upload

---
+++ Forfor.java
@@ -0,0 +1,21 @@
+public class Forfor {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("nei  ceng zhi qian");
+
+            for (int j = 0; j <=3; j++) {
+                System.out.println(i + "," + j);
+            }
+            System.out.println("nei ceng zhi hou");
+        }
+
+
+
+    }
+
+
+
+
+}
