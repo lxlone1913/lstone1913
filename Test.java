0f05a8250fbcc159e4c0da355b615e0900ddd2b5
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 10:44:10 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,23 +1,64 @@
-package test;
+package Demo001;
+class Teacher{
+  private String  Name;
+      private int   Age;
+      private  String Gender;
 
-public class Test {
-    public static void main(String[] args) {
-       Teacher tcher = new Teacher();
-       tcher.setName("aaa");
-       tcher.setTitle("bbb");
-        System.out.println("姓名" + tcher.getName);
+    public Teacher() {
+    }
 
+    public Teacher(String name, int age, String gender) {
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
 
+    public String getName() {
+        return Name;
+    }
 
+    public void setName(String name) {
+        Name = name;
+    }
 
+    public int getAge() {
+        return Age;
+    }
 
+    public void setAge(int age) {
+        Age = age;
+    }
 
+    public String getGender() {
+        return Gender;
+    }
 
-
-
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+    public void ShowInfo(){
+        System.out.println("name:"+getName());
+        System.out.println("name:"+getAge());
+        System.out.println("name:"+getGender());
 
     }
 
+
+
+
+}
+public class Test {
+    public static void main(String[] args) {
+
+        Teacher t= new Teacher();
+        t.setAge(78);
+        t.getAge();
+        t.setName("ddd");
+         t.getName();
+         t.setGender("man");
+         t.getGender();
+t.ShowInfo();
+    }
 
 
 
