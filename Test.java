e8c5fc02ad6c4c5c6f6fefa7fbae8d00f4b9dd5f
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 10:45:33 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,25 +1,30 @@
-package Demo002;
+package Demo003;
 
-class Bird{
-    void fly(){
-        Wing left = new Wing ();
-        Wing right = new Wing();
-        System.out.println(left + "" + right + "!!");
-    }
-
-}
-class Wing{}
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        Bird b = new Bird();
-        b.fly();
-        b.fly();
-        b.fly();
+        String s = null;
 
+           try {
+               char c =s.charAt(0);
+           }catch (Exception e){
+        System.out.println("c");}
+
+        System.out.println("hello");
+        System.out.println(100);
+        System.out.println(12345678901230L);
+        System.out.println(123444);
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("6");
+        System.out.println(true);
+        System.out.println(true);
+        System.out.println("qwr");
+        System.out.println(123);
 
     }
 
 
-
+    private static class Char {
+    }
 }
