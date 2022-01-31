dab39c1f164b16417362a5882ebf7b29f9cea12d
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 10:58:23 +0800

    Add files via upload

---
+++ Test11.java
@@ -0,0 +1,29 @@
+package Demo011;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) throws Exception{
+        String s="1a刘";
+        byte[]bb=s.getBytes(StandardCharsets.UTF_8);
+        System.out.println(Arrays.toString(bb));
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
