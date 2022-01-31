a982f733d7821a93bded15c7edb3209bd030f12a
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 10:41:23 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,33 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+       Teacher tcher = new Teacher();
+       tcher.setName("aaa");
+       tcher.setTitle("bbb");
+        System.out.println("姓名" + tcher.getName);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
