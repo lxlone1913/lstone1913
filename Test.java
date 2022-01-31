6bf96f60f10b2727aa65d1502a3bd2dc8b8e57a1
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 10:44:57 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,73 +1,24 @@
-package Demo001;
-class Teacher{
-  private String  Name;
-      private int   Age;
-      private  String Gender;
+package Demo002;
 
-    public Teacher() {
+class Bird{
+    void fly(){
+        Wing left = new Wing ();
+        Wing right = new Wing();
+        System.out.println(left + "" + right + "!!");
     }
-
-    public Teacher(String name, int age, String gender) {
-        Name = name;
-        Age = age;
-        Gender = gender;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public int getAge() {
-        return Age;
-    }
-
-    public void setAge(int age) {
-        Age = age;
-    }
-
-    public String getGender() {
-        return Gender;
-    }
-
-    public void setGender(String gender) {
-        Gender = gender;
-    }
-    public void ShowInfo(){
-        System.out.println("name:"+getName());
-        System.out.println("name:"+getAge());
-        System.out.println("name:"+getGender());
-
-    }
-
-
-
 
 }
+class Wing{}
+
 public class Test {
     public static void main(String[] args) {
+        Bird b = new Bird();
+        b.fly();
+        b.fly();
+        b.fly();
 
-        Teacher t= new Teacher();
-        t.setAge(78);
-        t.getAge();
-        t.setName("ddd");
-         t.getName();
-         t.setGender("man");
-         t.getGender();
-t.ShowInfo();
+
     }
-
-
-
-
-
-
-
-
-
 
 
 
