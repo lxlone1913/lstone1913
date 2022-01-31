bbb3fc3a94c8175683258cb74592215be2b5ad26
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 11:21:00 +0800

    Add files via upload

---
+++ Test0021.java
@@ -0,0 +1,25 @@
+package com.exercise02;
+
+public class Test002 {
+
+//    pvsm
+//    pvsm
+//    pvsm
+//    pvsm
+//    pvsm
+
+    public static void main(String[] args) {
+
+        System.out.println("lllllll");
+        System.out.println("lllllll");
+        System.out.println("lllllll");
+        System.out.println("lllllll");
+//        Test003.main(args);
+        System.out.println("lllllll");
+        System.out.println("lllllll");
+    }
+
+
+
+
+}
---
+++ Test0031.java
@@ -0,0 +1,16 @@
+package com.exercise02;
+
+public class Test003 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hhhhhhhhh");
+
+        System.out.println("学习学习的秘诀");
+             Test002.main(args);
+
+    }
+
+
+
+}
