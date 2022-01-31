b9e1c3d3c2c81f1a4703ec5b3b7867b519b841f9
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-01-31 10:51:37 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,17 +1,18 @@
-package Demo004;
+    package Demo005;
+
 
 import java.io.FileNotFoundException;
 
 public class Test {
 
-    static void f1()throws FileNotFoundException{
+    static void f1(){
         f2();
     }
-    static void f2()throws FileNotFoundException{
+    static void f2(){
         f3();
     }
-    static void f3()throws FileNotFoundException
-    {throw new FileNotFoundException();
+    static void f3(){
+        System.out.println(8/0);
     }
 
 
@@ -20,13 +21,15 @@ public class Test {
 
 
     public static void main(String[] args) {
-try {
-    f1();
-}catch (Exception e){
-    System.out.println("This is an error");
-}
-        {throw new RuntimeException();}
+        try {
+            f1();
+        }catch (Exception e){
+        e.printStackTrace();
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
 
 
     }
 }
+
